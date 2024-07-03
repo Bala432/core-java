1c02605656d31944afeb7d1392908407540a4e35
Bala Viswanadh <balaviswanadh21@gmail.com>
2024-07-03 21:09:28 +0530

    Initial Classes

---
+++ DataTypes.java
@@ -0,0 +1,10 @@
+class DataTypes
+{
+    public static void main(String []args)
+    {
+        int number1 = 2;
+        int number2 = 5;
+        int numbers_sum = number1 + number2 ;
+        System.out.println(numbers_sum);
+    }
+}
